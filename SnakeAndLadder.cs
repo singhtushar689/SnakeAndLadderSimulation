579876c24b25ed58cac4c2fa7ea48de5146b306c
singhtushar689 <singhtushar689@gmail.com>
2022-12-19 23:19:10 +0530

    Number Of Dice Rolled

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -8,43 +8,52 @@ namespace SnakeAndLadderSimulation
 {
     public class SnakeAndLadder
     {
-        public const int ladder = 0;
-        public const int snake = 1;
-        public const int win = 100;
+        public const int Ladder = 0;
+        public const int Snake = 1;
+        public const int Win = 100;
         public void Game()
         {
             int position = 0;
+            int diceCount = 0;
             Console.WriteLine("Enter the Snake and Ladder Game");
-            while (position < win)
+            while (position < Win)
             {
                 Random random = new Random();
                 int rollingDice = random.Next(1, 7);
-                int checkOption = random.Next(0, 3);
+                int checkOption = random.Next(3);
                 switch (checkOption)
                 {
-                    case ladder:
+                    case Ladder:
                         if (position + rollingDice <= 100)
-                        { 
-                        position += rollingDice;
+                        {
+                            Console.WriteLine("It is Ladder");
+                            position += rollingDice;
                         }
                         break;
-                    case snake:
+                    case Snake:
                         if(position - rollingDice < 1)
                         {
+                            Console.WriteLine("It is Snake ");
                             position = 0;
                         }
                         else
                         {
+                            Console.WriteLine("It is Snake ");
                             position -= rollingDice;
                         }
                         break;
                     default:
                         break;       
                 }
+                diceCount++;
                 Console.WriteLine("Dice Value is {0}",rollingDice);
+
+                
                 Console.WriteLine("It is {0}", checkOption);
                 Console.WriteLine("Player is at {0}",position);
+                
             }
+            Console.WriteLine("Number of times dice rolled : {0}", diceCount);
         }
     }
 }
