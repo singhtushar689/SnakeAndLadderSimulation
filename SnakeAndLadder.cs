cb783efdcda5e189c1507781cc61d120e883ba40
singhtushar689 <singhtushar689@gmail.com>
2022-12-19 22:15:24 +0530

    Switch Option

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -7,18 +7,41 @@ using System.Threading.Tasks;
 namespace SnakeAndLadderSimulation
 {
     public class SnakeAndLadder
-    { 
+    {
+        public const int ladder = 1;
+        public const int snake = 2;
+        public const int win = 100;
         public void Game()
         {
-            
-            int initialPosition = 0;
+            int position = 0;
             Console.WriteLine("Enter the Snake and Ladder Game");
-            Console.WriteLine("Player is at position {0}", initialPosition);
-
-            Random random = new Random();
-            int rollingDice = random.Next(1, 7);
-            Console.WriteLine("Number Shown randomly on dice is {0}",rollingDice);
-
+            while (position < win)
+            {
+                Random random = new Random();
+                int rollingDice = random.Next(1, 7);
+                int checkOption = random.Next(0, 3);
+                switch (checkOption)
+                {
+                    case ladder:                    
+                        position += rollingDice;
+                        break;
+                    case snake:
+                        if(position - rollingDice < 1)
+                        {
+                            position = 0;
+                        }
+                        else
+                        {
+                            position -= rollingDice;
+                        }
+                        break;
+                    default:
+                        break;       
+                }
+                Console.WriteLine("Dice Value is {0}",rollingDice);
+                Console.WriteLine("It is {0}", checkOption);
+                Console.WriteLine("Player is at {0}",position);
+            }
         }
     }
 }
