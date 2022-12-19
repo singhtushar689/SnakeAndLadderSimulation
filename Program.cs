fcccf9ee3a0b798964165b3c1047f2523216cbf5
singhtushar689 <singhtushar689@gmail.com>
2022-12-19 15:04:54 +0530

    Initial Position of Player Starting From 0

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SnakeAndLadder game = new SnakeAndLadder();
+            game.Game();
         }
     }
 }
---
+++ SnakeAndLadder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderSimulation
+{
+    public class SnakeAndLadder
+    { 
+        public void Game()
+        {
+            
+            int initialPosition = 0;
+            Console.WriteLine("Enter the Snake and Ladder Game");
+            Console.WriteLine("Player is at position {0}", initialPosition);
+
+        }
+    }
+}
